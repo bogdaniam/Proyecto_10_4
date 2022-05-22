260dc27d7d6593175ff4a8155d9add39528d82c6
bogdaniam <mihai.bogdan.mardale@gmail.com>
2022-05-22 13:40:15 +0200

    Mejoras en el JavaScript

--- Proyecto_10_4.js
+++ Proyecto_10_4.js
@@ -22,64 +22,66 @@ function generar() {
     function prueba(a) {
         function cuadroConRertraso() {
             //para colocar la imagen en cu cuadraito
+            //para poner el gif y el texto por defecto
             document.getElementById(`hijo${contador}`).src = `${a.message}`
+            document.getElementById(`nuevoPerro`).src = "https://1478500360.rsc.cdn77.org/app/00000001/1888/a5f164bb6f42c8b99b34ec41a2109cca9814e761_1607449794.gif"
+            document.getElementById("mensaje").innerText = "Gotta Catch 'Em All!"
         }
 
 
         //vamos a mostrar el nuevo perro encontrado
         document.getElementById(`nuevoPerro`).src = `${a.message}`
 
-        //https://images.dog.ceo/breeds/ridgeback-rhodesian/n02087394_8260.jpg
-
-        //para poner el gif y el texto por defecto
-        function changeSurprize() {
-            document.getElementById(`nuevoPerro`).src = "https://1478500360.rsc.cdn77.org/app/00000001/1888/a5f164bb6f42c8b99b34ec41a2109cca9814e761_1607449794.gif"
-            document.getElementById("mensaje").innerText = "Gotta Catch 'Em All!"
-        }
-
-        //despues de poner en pantalla el nuevo perro, le damos 2 segundo y vamos a poner el gif y el texto por defecto
-        setTimeout(changeSurprize, 1000);
+        
+        //despues de poner en pantalla el nuevo perro, le damos 1 segundo y vamos a poner el gif y el texto por defecto
         setTimeout(cuadroConRertraso, 1000);
         document.getElementById("restante").innerText = `Te quedan para descubrit ${20 - contador} Firulais`
 
 
-        //separamos el enlace, para luego luego saber cual es la raza
+        //separamos el enlace, para luego saber cual es la raza
         var enlaceSeparado = (a.message.split('/'));
 
         //declaramos una variable, y la ponemos a false
         let encontrado = false;
         let key;
+
+        //para poner el nombre bien
+        let nombreSplit = enlaceSeparado[4].split("-");
+        let nombreBienCompleto = "";
+        for (let j = 0; j < nombreSplit.length; j++) {
+            nombreBienCompleto += `${nombreSplit[j][0].toUpperCase()}${nombreSplit[j].substring(1)} `
+
+        }
+
+        
         //vamos a recorrer el locar storage, y mirar en cada key si tenemos ya la raza
         for (let i = 0; i <= localStorage.length; i++) {
             key = localStorage.key(i);
             //comprobacion del key con la nueva raza, si existe, vamos a guardar la cantidad de dicha raza, le sumamos uno y vamos a cambiar su valor en local storage
-            if (key == enlaceSeparado[4]) {
+            if (key == nombreBienCompleto) {
                 let cantNum = Number(JSON.parse(localStorage.getItem(`${key}`)).cantidad)
                 cantNum++;
-                localStorage.setItem(enlaceSeparado[4], JSON.stringify({
+                localStorage.setItem(nombreBienCompleto, JSON.stringify({
                     cantidad: cantNum,
                 }));
                 i = localStorage.length;
                 encontrado = true;
             }
         }
+       
+
         //si la raza todavia no existe en local storage, vamos a crear una nueva key y ponemos la cantidad al final
         if (!encontrado) {
-            localStorage.setItem(enlaceSeparado[4], JSON.stringify({
+            localStorage.setItem(nombreBienCompleto, JSON.stringify({
                 cantidad: 1,
             }));
         }
+        
 
-        //para poner el nombre bien en html
-        let myArraySplit1 = enlaceSeparado[4].split("-");
-        let myArraySplitUnido1 = "";
-        for (let j = 0; j < myArraySplit1.length; j++) {
-            myArraySplitUnido1 += `${myArraySplit1[j][0].toUpperCase()}${myArraySplit1[j].substring(1)} `
-
-        }
+        
 
         //cambiamos el texto del html
-        document.getElementById("mensaje").innerText = `Acabas de descubrir un... ${myArraySplitUnido1}`
+        document.getElementById("mensaje").innerText = `Acabas de descubrir un... ${nombreBienCompleto}`
         //console.log(enlaceSeparado)
 
         //añadir las razas y cantidad en array cuando se descubren los 20 perros
@@ -98,22 +100,13 @@ function generar() {
                 for (let i = 0; i < localStorage.length; i++) {
 
                     //Se guarda cada raza en la variabñe key
-                    key = localStorage.key(i);
-
-                    //Se crea un nuevo array por si acaso el nombre de la raza es compuesto y separado por -
-                    let myArraySplit = key.split("-");
-                    let myArraySplitUnido = "";
+                    //key = localStorage.key(i);
 
 
-                    //recorremos cada palabra del nombre de la raza, ponemos a uppercase la primeraletra y añadimos el resto de la palabra
-                    for (let j = 0; j < myArraySplit.length; j++) {
-                        myArraySplitUnido += `${myArraySplit[j][0].toUpperCase()}${myArraySplit[j].substring(1)} `
-
-                    }
-                    //console.log(myArraySplitUnido)
+                    //console.log(nombreBienCompleto)
                     //Se añade a los datos de los perros a la grafica
-                    razasFinal.push(myArraySplitUnido)
-                    razasCantidad.push(JSON.parse(localStorage.getItem(`${key}`)).cantidad)
+                    razasFinal.push(localStorage.key(i))
+                    razasCantidad.push(JSON.parse(localStorage.getItem(`${localStorage.key(i)}`)).cantidad)
                 }
 
 
